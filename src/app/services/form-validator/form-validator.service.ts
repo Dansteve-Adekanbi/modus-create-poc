48cc0acf2fb3c1731fbe44d2bc837b57815882f0
Dansteve <danstevea@gmail.com>
2022-08-29 06:32:50 +0100

    update services

--- src/app/services/form-validator/form-validator.service.ts
+++ src/app/services/form-validator/form-validator.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 import { isValidEmail, isValidName } from '@app-utils/form-validator';
-import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormValidatorService {
   constructor(
-    public translate: TranslateService,
   ) { }
 
   public static firstNameValidator(control: AbstractControl) {
@@ -38,12 +36,10 @@ export class FormValidatorService {
 
   public getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
     const errorMessages = {
-      required: this.translate.instant('FORM_VALIDATION.REQUIRED'),
-      invalidFirstName: this.translate.instant('FORM_VALIDATION.FIRST_NAME_ERROR'),
-      invalidLastName: this.translate.instant('FORM_VALIDATION.LAST_NAME_ERROR'),
-      invalidEmail: this.translate.instant('FORM_VALIDATION.EMAIL_ERROR'),
-      minlength: this.translate.instant('FORM_VALIDATION.MIN_LENGTH', { minLength: validatorValue.requiredLength }),
-      maxlength: this.translate.instant('FORM_VALIDATION.MAX_LENGTH', { maxLength: validatorValue.requiredLength }),
+      required: 'This field is required',
+      invalidEmail: 'Please enter a valid email address',
+      minlength: `minlength ${validatorValue.requiredLength}`,
+      maxlength: `maxlength ${validatorValue.requiredLength}`,
     };
 
     return errorMessages[validatorName];
